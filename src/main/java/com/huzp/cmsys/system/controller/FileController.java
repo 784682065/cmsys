7a0503090df11aefd2ddbdd7b25cda5dba9a43fb
huzp <784682065@qq.com>
2018-03-23 08:07:26 +0800

    xiugai

--- src/main/java/com/huzp/cmsys/system/controller/FileController.java
+++ src/main/java/com/huzp/cmsys/system/controller/FileController.java
@@ -125,6 +125,7 @@ public class FileController extends BaseController{
             myFile.setUsername(Integer.parseInt(ShiroKit.getUser().getUsername()));
 
             myFileDao.save(myFile);
+            System.out.println("1111");
 
             //因为文件名字可能有所变动所以重新创建dest
             dest = new File(filePath + fileName);
