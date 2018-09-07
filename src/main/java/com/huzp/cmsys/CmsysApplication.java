4e8e86a65a14e1476ecee27484d3164d0be96c81
huzp <784682065@qq.com>
2018-09-07 15:14:43 +0800

    第二次source测试 分支

--- src/main/java/com/huzp/cmsys/CmsysApplication.java
+++ src/main/java/com/huzp/cmsys/CmsysApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CmsysApplication {
 
 	public static void main(String[] args) {
+		System.out.println("第二次测试");
 		SpringApplication.run(CmsysApplication.class, args);
 	}
 }
